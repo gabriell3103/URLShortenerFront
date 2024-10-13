3c32246a9b887bc79e486533292979ae8aa200b2
gabriell3103 <g5819853@gmail.com>
2024-10-12 23:00:10 -0300

    Adicinando a lisense, o footer e o form do input

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="text-center w-full max-w-3xl">
-        <h1 className="text-5xl font-bold mb-4">URL Shortener</h1>
-        <h3 className="text-2xl font-bold mb-6 opacity-40">
-          Enter your long URL below and get a shortened link in seconds.
-        </h3>
-        <div className="flex flex-col items-center">
-          <input
-            type="text"
-            className="border border-gray-300 rounded p-2 text-lg mb-4 w-full max-w-md"
-            placeholder="Enter your URL"
-          />
-          <button className="w-full py-2 bg-black text-white text-lg rounded-md hover:bg-gray-800 max-w-md">
-            Shorten URL
-          </button>
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <div className="flex-grow flex items-center justify-center text-center w-full max-w-3xl mx-auto">
+        <div>
+          <h1 className="text-5xl font-bold mb-4">URL Shortener</h1>
+          <h3 className="text-2xl font-bold mb-6 opacity-40">
+            Enter your long URL below and get a shortened link in seconds.
+          </h3>
+          <div className="flex flex-col items-center">
+            <form className="w-full max-w-md mb-4">
+              <input
+                type="text"
+                className="border border-gray-300 rounded p-2 text-lg w-full"
+                placeholder="Enter your URL"
+              />
+            </form>
+            <button className="w-full py-2 bg-black text-white text-lg rounded-md hover:bg-gray-800 max-w-md">
+              Shorten URL
+            </button>
+          </div>
         </div>
       </div>
+
+      <footer className="fixed bottom-0 left-0 w-full bg-black text-white p-4">
+        <div className="flex justify-center items-center max-w-3xl mx-auto">
+          <p>&copy; 2024 Conheço uma Ponte. Todos os direitos reservados.</p>
+        </div>
+      </footer>
     </div>
   );
 }
