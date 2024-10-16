3ef04764832fd32b169f8d45c72bc1db432c4b7c
gabriell3103 <g5819853@gmail.com>
2024-10-15 23:21:29 -0300

    Integrando o front com o back, adicionando método POST

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App: React.FC = () => {
+  const [url, setUrl] = useState<string>('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const api_url = 'http://localhost:3333/shorten';
+
+    try {
+      const response = await fetch(api_url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          original_url: url,
+        }),
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Erro ao encurtar a URL:', error);
+    }
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <div className="flex-grow flex items-center justify-center text-center w-full max-w-3xl mx-auto">
@@ -10,16 +35,21 @@ function App() {
             Enter your long URL below and get a shortened link in seconds.
           </h3>
           <div className="flex flex-col items-center">
-            <form className="w-full max-w-md mb-4">
+            <form className="w-full max-w-md mb-4" onSubmit={handleSubmit}>
               <input
                 type="text"
                 className="border border-gray-300 rounded p-2 text-lg w-full"
                 placeholder="Enter your URL"
+                value={url}
+                onChange={e => setUrl(e.target.value)}
               />
+              <button
+                type="submit"
+                className="w-full py-2 bg-black text-white text-lg rounded-md hover:bg-gray-800 max-w-md mt-4"
+              >
+                Shorten URL
+              </button>
             </form>
-            <button className="w-full py-2 bg-black text-white text-lg rounded-md hover:bg-gray-800 max-w-md">
-              Shorten URL
-            </button>
           </div>
         </div>
       </div>
@@ -31,6 +61,6 @@ function App() {
       </footer>
     </div>
   );
-}
+};
 
 export default App;
